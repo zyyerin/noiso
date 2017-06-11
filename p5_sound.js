3fc0494330f81dd16ec6bac4b635daf380f64b59
Erin Zhang <yzhang3@cca.edu>
2017-06-10 18:46:08 -0700

    major update

--- p5_sound.js
+++ p5_sound.js
@@ -4,8 +4,8 @@ var amp;
 var w, sw;
 var a, astep;
 var sas;
-var strokeR, strokeG, strokeB, strokeAlpha;
-var offset;
+var strokeB, strokeAlpha;
+var offsetx, offsety;
 var relocationx, relocationy;
 var cfill;
 
@@ -13,149 +13,87 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
     angleMode(DEGREES);
+    
     mic = new p5.AudioIn();
     mic.start();
     amp = new p5.Amplitude();
     fft = new p5.FFT(0.9, 64);
     fft.setInput(mic);
-    w = width / 32;
+    w = random(100, 400);
+    
     // angle
     a = 0;
-    astep = 0.2;
-    sw = random(1);
+    astep = random(1)-0.5;
+    sw = astep;
     strokeWeight(sw);
     rectMode(CENTER);
     strokeAlpha = 255;
     sas = -1;
+    
     // color
     strokeB = 255;
-    offset = 0.1;
+    offsetx = 0.2;
+    offsety = 0.1;
     relocationx = 0;
     relocationy = 0;
     cfill = 255;
     
-     noisoIndex = 0;
+    noisoIndex = 0;
 }
 
 function draw() {
-    if(keyIsDown(DOWN_ARROW)) {
-        noisb();
-    } else if(keyIsDown(UP_ARROW)) {
-        noisc ();
-    }
-    
-    else{
     noisa();
-    }
 }
 
 function noisa() {
-    var spectrum = fft.analyze();
-    var vol = mic.getLevel();
-    var alpha = map(vol, 0, 1, 50, 255);
+    let spectrum = fft.analyze();
+    let vol = mic.getLevel();
+    let alpha = map(vol, 0, 1, 50, 255);
     stroke(255 - cfill, strokeAlpha / 2);
     strokeAlpha += sas;
     if (strokeAlpha <= 0) {
         sas *= -1;
     }
     fill(cfill, alpha);
-    offset += 0.002;
+    offsetx += 0.002;
+    offsety += 0.002;
     push();
-    translate(width * noise(offset), height * noise(offset));
+    translate(width/2 + noise(offsetx, offsety), height/2 + noise(offsetx, offsety));
     rotate(a);
     a += astep;
-    if (a >= 360) {
-        a = 0;
+    if (a > 1000 || a < -1000) {
+        astep *= -1;
     }
-    for (var i = 0; i <= spectrum.length; i++) {
-        var amp = spectrum[i];
-        var y = map(amp, 0, 255, height / 3, 0);
-        var d = map(a, 0, 360, 0, w);
+//    console.log(a);
+    for (var i = 0; i <= spectrum.length; i+=1) {
+        let amp = spectrum[i];
+        let y = map(amp, 50, 255, height / 4, 0);
+        let minw;
+        if (w>150) {
+            minw = w/5;
+        }else {
+            minw = w*2;
+        }
+        let d = map(amp, 0, 255, minw, w);
         push();
         rotate(a);
-        rect(-y / 2 * 1.4 + w / 2 + i * random(1) + w, 0, d * noise(a), y);
-        stroke(cfill);
-        rect(-y / 2 * 1.4 + w / 2 + i * random(1) + w, 0, y * noise(a) * 2, 1);
-        //        line(- y*1.4 + w / 2 + i * random(1) + w, d * noise(a));
+        stroke(255-cfill);
+        ellipse(-y/2 * 1.4 + i + minw, 0, d, y);
+        fill(255-cfill);
+        ellipse(-y/2 * 1.4 + i + minw, 0, 5, 5);
         pop();
     }
     pop();
     if (mouseIsPressed) {
         astep *= -1;
         cfill *= -1;
-        //    strokeG = random(255);
-        //    strokeB = random(255);
+        a += 5;
+    } else {
+        a += astep;
     }
 }
-
-function noisb() {
-    var spectrum = fft.analyze();
-    var vol = mic.getLevel();
-    var alpha = map(vol, 0, 1, 50, 255);
-    stroke(0, strokeAlpha);
-    strokeAlpha += sas;
-    if (strokeAlpha <= 0) {
-        sas *= -1;
-    }
-    fill(255 - strokeR, 255 - strokeG, 255 - strokeB, alpha);
-    offset += 0.002;
-    push();
-    translate(width * noise(offset), height * noise(offset));
-    rotate(a);
-    a += astep;
-    if (a >= 360) {
-        a = 0;
-    }
-    for (var i = 0; i <= spectrum.length; i++) {
-        var amp = spectrum[i];
-        var y = map(amp, 0, 255, 0, height / 2);
-        var d = map(a, 0, 360, 0, w);
-        push();
-        rotate(a);
-        rect(-y + w / 2 + i * random(1) + w, 0, d * noise(a) + sw, y);
-        pop();
-    }
-    pop();
-    if (mouseIsPressed) {
-        astep *= -1;
-        //    strokeG = random(255);
-        //    strokeB = random(255);
-    }
-}
-
-function noisc() {
-    var spectrum = fft.analyze();
-    var vol = mic.getLevel();
-    var alpha = map(vol, 0, 1, 50, 255);
-    stroke(0, strokeAlpha);
-    strokeAlpha += sas;
-    if (strokeAlpha <= 0) {
-        sas *= -1;
-    }
-    fill(255 - strokeR, 255 - strokeG, 255 - strokeB, alpha);
-    offset += 0.002;
-    push();
-    translate(width / 2 * noise(offset), height / 2 * noise(offset));
-    rotate(a);
-    a += astep;
-    if (a >= 360) {
-        a = 0;
-    }
-
-    for (var i = 0; i <= spectrum.length; i++) {
-        var amp = spectrum[i];
-        var y = map(amp, 0, 255, 0, height / 2);
-        var d = map(a, 0, 360, 0, w);
-
-        push();
-        rotate(a);
-        rect(- y + w / 2 + i * random(10) + w, 0, d * noise(a) + sw*2, y);
-        pop();
-    }
-    pop();
-}
-
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    background(0);
 }
